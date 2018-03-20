93af4d76374a92e4428fcff413d088844139ec01
zhanglinling <zhanglinling0926@gmail.com>
2018-03-21 00:33:52 +0800

    拆分组件 Book
    拆分出组件 Book

---
+++ src/Book.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Book extends React.Component {
+  static propTypes = {
+    img: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    authors: PropTypes.string.isRequired,
+  };
+
+  state = {}
+
+  render() {
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("' + this.props.img + '")' }}></div>
+          <div className="book-shelf-changer">
+            <select>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{this.props.title}</div>
+        <div className="book-authors">{this.props.authors}</div>
+      </div>
+    );
+  }
+}
