b0fd66917934122dec7a20175e4b3e5841127e17
zhanglinling <zhanglinling0926@gmail.com>
2018-03-21 15:15:47 +0800

    拆分组件
    拆分出 Search Bookshelf

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { Link, Route } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 // import * as BooksAPI from './BooksAPI'
 import './App.css';
-import Book from './Book'
+import Bookshelf from './Bookshelf'
+import Search from './Search'
 
 class BooksApp extends React.Component {
   state = {
@@ -51,22 +52,9 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
         <Route path="/search" exact render={() => (
+
           <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
+            <Search />
             <div className="search-books-results">
               <ol className="books-grid"></ol>
             </div>
@@ -79,46 +67,13 @@ class BooksApp extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      {this.state.currentlyReading.map((book) => (
-                        <li>
-                          <Book img={book.img} title={book.title} authors={book.authors} />
-                        </li>
-                      ))}
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      {this.state.wantToRead.map((book) => (
-                        <li>
-                          <Book img={book.img} title={book.title} authors={book.authors} />
-                        </li>
-                      ))}
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      {this.state.read.map((book) => (
-                        <li>
-                          <Book img={book.img} title={book.title} authors={book.authors} />
-                        </li>
-                      ))}
-                    </ol>
-                  </div>
-                </div>
+                <Bookshelf shelftitle="Currently Reading" books={this.state.currentlyReading}/>
+                <Bookshelf shelftitle="Want to Read" books={this.state.wantToRead}/>
+                <Bookshelf shelftitle="Read" books={this.state.read}/>
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <Link to="/search">Add a book</Link>
             </div>
           </div>
         )} />
---
+++ src/Bookshelf.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Book from './Book';
+
+export default class Bookshelf extends React.Component {
+  static propTypes = {
+    shelftitle: PropTypes.string.isRequired,
+    books: PropTypes.array.isRequired
+  };
+
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.shelftitle}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.books.map((book, index) => (
+              <li key={index}>
+                <Book img={book.img} title={book.title} authors={book.authors} />
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
